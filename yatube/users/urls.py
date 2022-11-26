b49e6ee2c1d9dfa381659eda659ee5584900540b
Gor Galstyan <gorqwertyuiop@mail.ru>
2022-11-26 12:29:58 +0300

    pep8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -31,28 +31,25 @@ urlpatterns = [
             template_name='users/password_change_done.html'),
         name='password_change_done'
     ),
-    # ?
+    
     path(
         'password_reset/',
         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
         name='password_reset'
     ),
-    #?
     path(
         'password_reset/done/',
         PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'),
         name='password_reset_done'
     ),
-    #?
     path(
         'reset/done/',
         PasswordResetCompleteView.as_view(
             template_name='users/password_reset_complete.html'),
         name='password_reset_complete'
     ),
-    #?
     path(
         'reset/<uidb64>/token/',
         PasswordResetConfirmView.as_view(
