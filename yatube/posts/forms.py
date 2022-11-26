0cee4406ebc62a2d60832faba834f494129c497f
Gor Galstyan <gorqwertyuiop@mail.ru>
2022-11-26 18:58:58 +0300

    ready

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,4 +7,5 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
-        help_text = {'text': 'Введите текст', 'group': 'Выберите группу'}
+        label = {'text':'Введите текст', 'group':'Выберите группу'}
+        help_text = {'text': 'Пост который хотите добавить или редактировать', 'group': 'Из уже существующих'}
